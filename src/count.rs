91ac8307ff91dade2e7293d3d2c836f15ac88ead
Stefan Lukas <etaloof@gmail.com>
2023-10-16 00:18:55 +0200

    Add tests for counter

--- src/count.rs
+++ src/count.rs
@@ -5,7 +5,7 @@ pub struct Counter {
 }
 
 impl Counter {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self { counter: 0 }
     }
 
@@ -20,5 +20,17 @@ impl Write for Counter {
     fn write_str(&mut self, s: &str) -> std::fmt::Result {
         self.counter += s.len();
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_counter() {
+        assert_eq!(Counter::count(""), 0);
+        assert_eq!(Counter::count("hello"), 5);
+        assert_eq!(Counter::count("hello world"), 11);
     }
 }
