b24c2987357be80712fcdf04929bf10631564ccd
Stefan Lukas <etaloof@gmail.com>
2023-12-09 00:29:47 +0100

    Remove Clone bound on Iterator in DisplayIteratorJoined

--- src/joined.rs
+++ src/joined.rs
@@ -1,43 +1,43 @@
-use std::fmt::{Display, Formatter};
+use std::{
+    cell::Cell,
+    fmt::{Display, Formatter},
+};
 
 use crate::utils::LastIterationIterator;
 
 pub struct DisplayIteratorJoined<Iter, I, S>
 where
-    Iter: Iterator<Item = I> + Clone,
+    Iter: Iterator<Item = I>,
     I: Display,
     S: Display,
 {
-    iter: Iter,
+    iter: Cell<Option<Iter>>,
     separator: S,
 }
 
 impl<Iter, I, S> DisplayIteratorJoined<Iter, I, S>
 where
-    Iter: Iterator<Item = I> + Clone,
+    Iter: Iterator<Item = I>,
     I: Display,
     S: Display,
 {
     pub fn new<II: IntoIterator<IntoIter = Iter>>(iter: II, separator: S) -> Self {
         Self {
-            iter: iter.into_iter(),
+            iter: Cell::new(Some(iter.into_iter())),
             separator,
         }
-    }
-
-    pub fn iter(&self) -> Iter {
-        self.iter.clone()
     }
 }
 
 impl<Iter, I, S> Display for DisplayIteratorJoined<Iter, I, S>
 where
-    Iter: Iterator<Item = I> + Clone,
+    Iter: Iterator<Item = I>,
     I: Display,
     S: Display,
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for (is_last_iteration, item) in LastIterationIterator::new(self.iter()) {
+        let iter = self.iter.replace(None).ok_or(Default::default())?;
+        for (is_last_iteration, item) in LastIterationIterator::new(iter) {
             if is_last_iteration {
                 write!(f, "{}", item)?;
             } else {
