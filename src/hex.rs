d4bef4e3ea9277bd90e618efc032f724f0b1cf26
Stefan Lukas <etaloof@gmail.com>
2023-12-09 19:02:49 +0100

    Add LowerHex and UpperHex

--- src/hex.rs
+++ src/hex.rs
@@ -1,20 +1,49 @@
-use std::fmt::{Display, Formatter, LowerHex};
+use std::fmt::{Display, Formatter};
 
-pub struct Hex<I: LowerHex> {
+/// Type alias for a `LowerHex`.
+pub type Hex<I> = LowerHex<I>;
+
+/// A wrapper type for formatting values as hexadecimal strings with a specific case.
+pub struct LowerHex<I: std::fmt::LowerHex> {
     item: I,
 }
 
-impl<I: LowerHex> Hex<I>
+impl<I: std::fmt::LowerHex> LowerHex<I>
 where
-    I: LowerHex,
+    I: std::fmt::LowerHex,
 {
+    /// Creates a new `LowerHex` value with the given value.
     pub fn new(item: I) -> Self {
         Self { item }
     }
 }
 
-impl<I: LowerHex> Display for Hex<I> {
+impl<I: std::fmt::LowerHex> Display for LowerHex<I> {
+    /// Formats the value as lowercase hexadecimal.
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        self.item.fmt(f)
+        std::fmt::LowerHex::fmt(&self.item, f)
+    }
+}
+
+/// A wrapper type for formatting values as hexadecimal strings with a specific case.
+pub struct UpperHex<I: std::fmt::UpperHex> {
+    /// The value that is being formatted.
+    item: I,
+}
+
+impl<I: std::fmt::UpperHex> UpperHex<I>
+where
+    I: std::fmt::UpperHex,
+{
+    /// Creates a new `UpperHex` value with the given value.
+    pub fn new(item: I) -> Self {
+        Self { item }
+    }
+}
+
+impl<I: std::fmt::UpperHex> Display for UpperHex<I> {
+    /// Formats the value as uppercase hexadecimal.
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        std::fmt::UpperHex::fmt(&self.item, f)
     }
 }
