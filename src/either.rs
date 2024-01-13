ca4b0fd5b9f50ee80e0e09ac4db2880020bad660
Stefan Lukas <etaloof@gmail.com>
2024-01-13 01:25:29 +0100

    Document Either

--- src/either.rs
+++ src/either.rs
@@ -1,15 +1,34 @@
 use std::fmt::{Display, Formatter};
 
-pub enum Either<L: Display, R: Display> {
+/// An enum that represents values of two different types.
+///
+/// # Examples
+///
+/// ```rust
+/// # use fmt::either::Either;
+///
+/// let left: Either<_, String> = Either::new_left("Hello, World!");
+///
+/// match left {
+///     Either::Left(value) => println!("The left value is: {}", value),
+///     Either::Right(value) => unreachable!("There is no right value!"),
+/// }
+/// ```
+#[derive(Debug)]
+pub enum Either<L, R> {
+    /// The left value.
     Left(L),
+    /// The right value.
     Right(R),
 }
 
-impl<L: Display, R: Display> Either<L, R> {
+impl<L, R> Either<L, R> {
+    /// Creates a new `Either` with a left value.
     pub fn new_left(a: L) -> Self {
         Either::Left(a)
     }
 
+    /// Creates a new `Either` with a right value.
     pub fn new_right(b: R) -> Self {
         Either::Right(b)
     }
