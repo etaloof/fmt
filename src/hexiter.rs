757bff38860bdedb0bc80ce28451462f3668cd0d
Stefan Lukas <etaloof@gmail.com>
2024-03-15 18:59:02 +0100

    WIP hexiter v2

--- src/hexiter.rs
+++ src/hexiter.rs
@@ -1,53 +1,69 @@
-use std::{
-    cell::Cell,
-    fmt::{Display, Formatter, LowerHex},
-    iter::Map,
-};
+use std::fmt::{Display, Formatter, LowerHex};
 
-use crate::{adhoc2::Disp, joined::DisplayIteratorJoined};
+use crate::{adhoc2::Disp, joined::DisplayIteratorJoined, producer::DisplayProducer};
 
-type MapFunc<I> = fn(<I as Iterator>::Item) -> Disp<<I as Iterator>::Item>;
+trait IteratorProducer {
+    /// The iterator produced by this IteratorProducer.
+    type Iterator: Iterator;
 
-pub struct HexIter<I>
-where
-    I: Iterator,
-    I::Item: LowerHex,
-{
-    item: Cell<Option<DisplayIteratorJoined<Map<I, MapFunc<I>>, &'static str>>>,
+    fn produce_iterator(&self) -> Self::Iterator;
 }
 
-impl<I> HexIter<I>
-where
-    I: Iterator,
-    I::Item: LowerHex,
-{
-    pub fn new<J>(iter: J) -> Self
-    where
-        J: IntoIterator<IntoIter = I>,
-    {
-        let map: MapFunc<I> = |x| Disp::new(x, LowerHex::fmt);
-        let iter = iter.into_iter().map(map);
-        let iter = DisplayIteratorJoined::new(iter, ", ");
-        Self {
-            item: Cell::new(Some(iter)),
-        }
+impl<I: Iterator, F: Fn() -> I> IteratorProducer for F {
+    type Iterator = I;
+
+    fn produce_iterator(&self) -> Self::Iterator {
+        self()
     }
 }
 
-impl<I> Display for HexIter<I>
+impl<'a, T> IteratorProducer for &'a [T]
 where
-    I: Iterator,
-    I::Item: LowerHex,
+    T: LowerHex,
+{
+    type Iterator = std::slice::Iter<'a, T>;
+
+    fn produce_iterator(&self) -> Self::Iterator {
+        self.into_iter()
+    }
+}
+
+type MapFunc<I> = fn(<I as Iterator>::Item) -> Disp<<I as Iterator>::Item>;
+
+pub struct HexIter<J>
+where
+    J: IteratorProducer,
+    <<J as IteratorProducer>::Iterator as IntoIterator>::Item: LowerHex,
+{
+    iter: J,
+}
+
+impl<J> HexIter<J>
+where
+    J: IteratorProducer,
+    <<J as IteratorProducer>::Iterator as IntoIterator>::Item: LowerHex,
+{
+    pub fn new(iter: J) -> Self {
+        Self { iter }
+    }
+}
+
+impl<J> Display for HexIter<J>
+where
+    J: IteratorProducer,
+    <<J as IteratorProducer>::Iterator as IntoIterator>::Item: LowerHex,
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let item = self.item.replace(None).ok_or(Default::default())?;
-        item.fmt(f)
+        let map: MapFunc<J::Iterator> = |x| Disp::new(x, LowerHex::fmt);
+        let iter = self.iter.produce_iterator().map(map);
+        let iter = DisplayIteratorJoined::new(iter, ", ");
+        iter.fmt(f)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::slice::Iter;
+    use std::fmt::Display;
 
     use super::*;
 
@@ -65,7 +81,7 @@ mod tests {
 
     #[test]
     fn test_empty_slice() {
-        let slice: HexIter<Iter<i32>> = HexIter::new(&[]);
+        let slice: HexIter<&[u32]> = HexIter::new(&[]);
         assert_eq!(slice.to_string(), "");
     }
 
@@ -73,5 +89,11 @@ mod tests {
     fn test_single_element_slice() {
         let slice = HexIter::new(&[0xa]);
         assert_eq!(slice.to_string(), "a");
+    }
+
+    #[test]
+    fn test_filtered_slice() {
+        let slice = HexIter::new([0xa, 0xb, 0xc].iter().filter(|x| *x & 0x1 == 0x1));
+        assert_eq!(slice.to_string(), "a, b");
     }
 }
